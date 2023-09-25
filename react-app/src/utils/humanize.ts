ef94fd3a1eb87f11549fa3b65d185a7e858be7d8
sheey11 <13375959+sheey11@users.noreply.github.com>
2023-09-25 12:26:05 +0800

    refactor: optimize filesize algorithm

--- react-app/src/utils/humanize.ts
+++ react-app/src/utils/humanize.ts
@@ -2,21 +2,12 @@ export function humanizeFileSize(bytes: number) {
     // note: use division rather than bit opration
     // to perserve decimals
 
-    // B, KB, MB, GB, TB
-    let magnitudes = []
     let i = 0
-    for (i = 0; i < 5; i++) magnitudes.push(bytes / Math.pow(2, i * 10))
-
-    const whosBetter = (a: number, b: number) => b < a && b > 0.9 ? 'latter' : 'former'
-
     let result = bytes
-    for (i = 1; i < 5; i++) {
-        if (whosBetter(magnitudes[i - 1], magnitudes[i]) === 'former') {
-            i = i - 1
-            break
-        } else {
-            result = magnitudes[i]
-        }
+    for (i = 0; i < 5; i++) {
+        let tmp = result / Math.pow(2, 10)
+        if (tmp < 0.9) break
+        else result = tmp
     }
 
     const units = ['B', 'KB', 'MB', 'GB', 'TB']
