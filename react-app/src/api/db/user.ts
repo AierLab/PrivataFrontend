c6d93031950a6edb2ec82b5bf55113493666aa93
Weidows <utsuko27@qq.com>
2023-10-08 11:06:53 +0800

    fix jest & update prisma.

--- react-app/src/api/db/user.ts
+++ react-app/src/api/db/user.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
 interface User {
   id: number;
@@ -16,19 +16,19 @@ interface Post {
 
 interface ChatHistory {
   chat_uid: string;
-  sender:   string;
-  message:  string;
+  sender: string;
+  message: string;
 }
 interface FileStoreInfo {
-  chat_uid: string 
-  file_path: string
+  chat_uid: string;
+  file_path: string;
 }
 
 const prisma = new PrismaClient();
 
 async function createUser(userData: User) {
   return await prisma.user.create({
-    data: userData
+    data: userData,
   });
 }
 
@@ -38,26 +38,26 @@ async function getUsers() {
 
 async function getUser(id: number) {
   return await prisma.user.findUnique({
-    where: { id }
+    where: { id },
   });
 }
 
 async function updateUser(id: number, userData: User) {
   return await prisma.user.update({
     where: { id },
-    data: userData
+    data: userData,
   });
 }
 
 async function deleteUser(id: number) {
   return await prisma.user.delete({
-    where: { id }
+    where: { id },
   });
 }
 
 async function createPost(postData: Post) {
   return await prisma.post.create({
-    data: postData
+    data: postData,
   });
 }
 
@@ -67,69 +67,22 @@ async function getPosts() {
 
 async function getPost(id: number) {
   return await prisma.post.findUnique({
-    where: { id }
+    where: { id },
   });
 }
 
 async function updatePost(id: number, postData: Post) {
   return await prisma.post.update({
     where: { id },
-    data: postData
+    data: postData,
   });
 }
 
 async function deletePost(id: number) {
   return await prisma.post.delete({
-    where: { id }
+    where: { id },
   });
 }
-
-async function createChat(history: ChatHistory) {
-  return await prisma.chatConversation.create({
-    data: history
-  });
-}
-
-async function deleteChat(chat_uid: string) {
-  return await prisma.chatConversation.deleteMany({
-    where: { chat_uid:  { equals: chat_uid } }
-  });
-}
-
-async function getChat(chat_uid: string) {
-  return await prisma.chatConversation.findMany({
-    where: { chat_uid: { equals: chat_uid }}
-  });
-}
-
-async function getAllChat(history: ChatHistory) {
-  return await prisma.chatConversation.findMany();
-}
-
-
-async function createFileInfo(file_info: FileStoreInfo) {
-  return await prisma.fileStore.create({
-    data: file_info
-  });
-}
-async function getFileInfo(chat_uid: string) {
-  return await prisma.fileStore.findMany({
-    where: { chat_uid: { equals: chat_uid }}
-  });
-}
-async function deleteFileInfo(chat_uid: string) {
-  return await prisma.fileStore.deleteMany({
-    where: { chat_uid: { equals: chat_uid }}
-  });
-}
-
-async function updateFileInfo(chat_uid: string, file_info: FileStoreInfo) {
-  return await prisma.fileStore.updateMany({
-    where: { chat_uid: { equals: chat_uid }},
-    data: file_info
-  });
-}
-
 
 export {
   User,
@@ -146,12 +99,4 @@ export {
   getPost,
   updatePost,
   deletePost,
-  createChat,
-  deleteChat,
-  getAllChat,
-  getChat,
-  createFileInfo,
-  getFileInfo,
-  deleteFileInfo,
-  updateFileInfo
 };
